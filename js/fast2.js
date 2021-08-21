51430a76cc75bc72894d98d9739cfa326edb6abb
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-08-21 15:37:37 +0530

    Update fast2.js

--- js/fast2.js
+++ js/fast2.js
@@ -20,7 +20,7 @@ function testVariable() {
     {rate=traveller;}
 
     var result = 'Booking from a1droptaxi.in' + '%0ACustomer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate;
-
+    var result = 'Booking from a1droptaxi.in' + '<br/>Customer Name:  ' + strText + '<br/>Phone Number: ' + strText1 +'<br/>Pickup Location: ' + strText2+  '<br/>Drop Location: ' + strText3 + '<br/>Pickup Date: ' + strText4 + '<br/>Pickup Time: ' + strText6 +'<br/>Service: ' + strText8 + '<br/>Cars: ' + strText7+"<br/>Price:"+rate;
 var finalMsg = encodeURI(result);
          document.getElementById("bookingForm").addEventListener("submit", (e) => {
 e.preventDefault();
@@ -31,22 +31,33 @@ if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6
         {
 	const request = new XMLHttpRequest();
       //  const url = 'https://api.telegram.org/bot1641457962:AAGEETFtrtqexe3xRiFbeoXqKO6xoP9YnC0/sendMessage?chat_id=-1001516712828&text='+result;
+     //request.open("post", url); 
+    //request.send();	
+   //alert("Booking Confirmed");
+  //window.open('bc.html', '_self');
+ //window.location.href = "bc.html";
+		                    Email.send({
+                Host:"smtp.gmail.com",
+                Username:'thereciprocalolutions.projects@gmail.com',
+                Password:'cgenxthsgpwplxoy',
+                To: 'thereciprocalsolutions@gmail.com',
+                From: 'thereciprocalolutions.projects@gmail.com',
+                Subject: 'New Enquiry From '+strText,
+                Body:'The New Enquriy Details'+result1+'<br/>',
+                })//.then(
+			//(message)=>alert("Your enquiry has been submitted.We will get back to you in 24 Hours.")
+				   // );
 		
-fetch('https://api.telegram.org/bot1641457962:AAGEETFtrtqexe3xRiFbeoXqKO6xoP9YnC0/sendMessage?chat_id=-1001516712828&text='+result)
+		
+//fetch('https://api.telegram.org/bot1641457962:AAGEETFtrtqexe3xRiFbeoXqKO6xoP9YnC0/sendMessage?chat_id=-1001516712828&text='+result)
 			.then(response=>{
-			if(response.status==200)
-			{
+				if(response.status==200)
+				{
 				console.log("Sent Message successfully");
-				
-	//request.open("post", url);
-	//request.send();
-	console.log('Message sent');
-        //alert("Booking Confirmed");
-        //window.open('bc.html', '_self');
-	setTimeout(function(){ window.open('https://a1droptaxi.in/bc.html', '_self'); }, 1000);
-	//window.location.href = "bc.html";
-			}
-})
+				console.log('Message sent');
+				setTimeout(function(){ window.open('https://a1droptaxi.in/bc.html', '_self'); }, 100);
+				}
+			      })
         }
 });
 }       
